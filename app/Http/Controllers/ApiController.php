3d92bcababb2c0fa43307908703e7567bd795947
korkut <erhankorkut@outlook.com>
2016-11-24 06:14:00 +0300

    hata düzeltme

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -11,17 +11,18 @@ class ApiController extends Controller
     public function login()
     {
         $user = User::where('email', Input::get('email'))->first();
-        if (Hash::check(Input::get('password'), $user->password)) {
+        if ($user && Hash::check(Input::get('password'), $user->password)) {
             return response()->json([
                 'case' => '1',
+                'mesaj' => 'islem basarili',
                 'token' => md5(Input::get('email') . Input::get('password'))
             ]);
         } else
             return response()->json([
                 'case' => '0',
-                'mesaj' => 'hatali giris'
+                'mesaj' => 'hatali giris',
+                'token' => ''
             ]);
-
     }
 
     public function register()
@@ -29,21 +30,23 @@ class ApiController extends Controller
         if (User::where('email', Input::get('email'))->first())
             return response()->json([
                 'case' => '0',
-                'mesaj' => 'uye kayitlidir'
+                'mesaj' => 'uye kayitlidir',
+                'token' => '',
             ]);
         else {
+            $token = (md5(Input::get('email') . Input::get('password')));
             $user = new User();
             $user->name = Input::get('name');
             $user->surname = Input::get('surname');
             $user->email = Input::get('email');
             $user->password = bcrypt(Input::get('password'));
             $user->phone_number = Input::get('phone_number');
-            $user->setRememberToken(md5(Input::get('email') . Input::get('password')));
+            $user->login_token = $token;
             $user->save();
             return response()->json([
                 'case' => '1',
                 'mesaj' => 'uye kayit basarili',
-                'token' => $user->getRememberToken()
+                'token' => $user->login_token
             ]);
         }
 
--- app/User.php
+++ app/User.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     protected $fillable = [
-        'name', 'surname', 'email', 'password', 'phone_number',
+        'name', 'surname', 'email', 'password', 'phone_number', 'login_token',
     ];
 
     /**
